c09fe388a58756415db938f7704a77fdbbc562fe
beike98 <49637524+beike98@users.noreply.github.com>
2021-07-27 22:26:55 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Baker />
+</template>
+
+<script>
+import Baker from "./components/Baker.vue";
+
+export default {
+  name: "App",
+  components: {
+    Baker,
+  },
+};
+</script>
