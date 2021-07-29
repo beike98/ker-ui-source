297459075b0d3d9df9b0bf6ec7715d8048ba17c1
beike98 <49637524+beike98@users.noreply.github.com>
2021-07-29 15:46:09 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,11 +2,17 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">toggle</Button>
-  <Dialog :visible="x" />
+  <Dialog
+    v-model:visible="x"
+    :closeOnClickOverlay="false"
+    :ok="f1"
+    :cancel="f2"
+  />
 </template>
 <script lang="ts">
 import Dialog from "../lib/Dialog.vue";
 import Button from "../lib/Button.vue";
+import { ref } from "vue";
 export default {
   components: {
     Dialog,
@@ -17,7 +23,11 @@ export default {
     const toggle = () => {
       x.value = !x.value;
     };
-    return { x, toggle };
+    const f1 = () => {
+      return false;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
   },
 };
 </script>
