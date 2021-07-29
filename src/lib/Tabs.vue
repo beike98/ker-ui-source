2e6378d0a3b528ba623537d1d41f00295943ea4b
beike98 <49637524+beike98@users.noreply.github.com>
2021-07-30 05:49:03 +0800

    Tabs组件-优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,7 +6,7 @@
           :class="{ selected: t === selected }"
           v-for="(t, index) in titles"
           :key="index"
-          :ref="el => { if (el) navItems[index] = el }"
+          :ref="el => { if (t===selected) selectedItem = el }"
           @click="select(t)"
       >
         {{ t }}
@@ -24,9 +24,10 @@
     </div>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts">var selectedItem;
+
 import Tab from "./Tab.vue";
-import {computed, onMounted, onUpdated, ref} from "vue";
+import {computed, onMounted, ref, watchEffect} from "vue";
 
 export default {
   props: {
@@ -35,21 +36,19 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
-    const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      const {width} = result.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {left: left1} = container.value.getBoundingClientRect()
-      const {left: left2} = result.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        const {width} = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {left: left1} = container.value.getBoundingClientRect()
+        const {left: left2} = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -59,11 +58,6 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    const current = computed(() => {
-      return defaults.filter((tag) => {
-        return tag.props.title === props.selected;
-      })[0];
-    });
     const select = (title: string) => {
       context.emit("update:selected", title);
     };
@@ -71,8 +65,7 @@ export default {
       defaults,
       titles,
       select,
-      current,
-      navItems,
+      selectedItem,
       indicator,
       container
     };
