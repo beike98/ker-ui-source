a04ee7090d66ad6535fcc76dbae6a9f6185f52bd
beike98 <49637524+beike98@users.noreply.github.com>
2021-07-31 12:14:55 +0800

    优化markdown

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import "./lib/gulu.scss";
 import "./index.scss";
 import "./lib/svg.js"
+import "github-markdown-css"
+import Markdown from './components/Markdown.vue';
+
 
 import { router } from './router';
 import { createApp } from "vue";
@@ -8,3 +11,4 @@ import App from "./App.vue";
 const app = createApp(App)
 app.use(router)
 app.mount("#app");
+app.component("Markdown", Markdown)
